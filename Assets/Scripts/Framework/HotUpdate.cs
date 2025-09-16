df1b79c59a865ddfae41593891781aa788ffc92c
UvUae <1873099584@qq.com>
2025-09-16 10:43:56 +0800

    two day

---
+++ Assets/Scripts/Framework/HotUpdate.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HotUpdate : MonoBehaviour
+{
+    byte[] m_ReadPathFileListData;
+    byte[] m_ServerFileListData;
+    internal class DownFileInfo
+    {
+        public string url;
+        public string fileName;
+        public DownloadHandler fileData;
+    }
+    //下载文件数量
+    int m_DownloadCount;
+
+
+
+    /// <summary>
+    /// 下载单个文件
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    IEnumerator DownLoadFile(DownFileInfo info,Action<DownFileInfo> Complete)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(info.url);
+        yield return webRequest.SendWebRequest();
+        if (webRequest.isHttpError || webRequest.isNetworkError)
+        {
+            Debug.LogError("下载文件出错：" + info.url);
+            yield break;
+            //重试
+        }
+        yield return new WaitForSeconds(0.2f);
+
+        info.fileData = webRequest.downloadHandler;
+        Complete?.Invoke(info);
+        webRequest.Dispose();
+    }
+
+    /// <summary>
+    /// 下载多个文件
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="Complete"></param>
+    /// <returns></returns>
+    IEnumerator DownLoadFile(List<DownFileInfo> infos, Action<DownFileInfo> Complete,Action DownLoadAllComplete)
+    {
+        foreach (DownFileInfo info in infos)
+        {
+            yield return DownLoadFile(info, Complete);
+        }
+        DownLoadAllComplete?.Invoke();
+    }
+
+    /// <summary>
+    /// 获取文件信息
+    /// </summary>
+    /// <returns></returns>
+    private List<DownFileInfo> GetFileList(string fileData,string path)
+    {
+        string content = fileData.Trim().Replace("\r", "");
+        string[] files = content.Split('\n');
+        List<DownFileInfo> downFileInfos = new List<DownFileInfo>(files.Length);
+        for (int i = 0; i < files.Length; i++)
+        {
+            string[] info = files[i].Split('|');
+            DownFileInfo fileInfo = new DownFileInfo();
+            fileInfo.fileName = info[1];
+            fileInfo.url = Path.Combine(path, info[1]);
+            downFileInfos.Add(fileInfo);
+        }
+        return downFileInfos;
+    }
+
+
+    GameObject loadingObj;
+    LoadingUI loadingUI;
+    private void Start()
+    {
+        GameObject go = Resources.Load<GameObject>("LoadingUI");
+        loadingObj = Instantiate(go);
+        loadingObj.transform.SetParent(this.transform);
+        loadingUI = loadingObj.GetComponent<LoadingUI>();
+
+        if (IsFirstInstall())
+        {
+            ReleaseResources();
+        }
+        else
+        {
+            CheckUpdate();
+        }
+    }
+
+    private bool IsFirstInstall()
+    {
+        //判断只读目录是否存在版本文件
+        bool isExistsReadPath = FileUtil.IsExists(Path.Combine(PathUtil.ReadPath, AppConst.FileListName));
+
+        //判断可读写目录是否存在版本文件
+        bool isExistsReadWritePath = FileUtil.IsExists(Path.Combine(PathUtil.ReadWritePath, AppConst.FileListName));
+
+        return isExistsReadPath && !isExistsReadWritePath;
+    }
+
+    private void ReleaseResources()
+    {
+        m_DownloadCount = 0;
+        string url = Path.Combine(PathUtil.ReadPath, AppConst.FileListName);
+        DownFileInfo info = new DownFileInfo();
+        info.url = url;
+        StartCoroutine(DownLoadFile(info, OnDownLoadReadPathFileListComplete));
+    }
+
+    private void OnDownLoadReadPathFileListComplete(DownFileInfo file)
+    {
+        m_ReadPathFileListData = file.fileData.data;
+        List<DownFileInfo> fileInfos = GetFileList(file.fileData.text, PathUtil.ReadPath);
+        StartCoroutine(DownLoadFile(fileInfos, OnReleaseFileComplete, OnReleaseAllFileComplete));
+        loadingUI.InitProgress(fileInfos.Count,"正在释放资源，不消耗流量...");
+    }
+
+    private void OnReleaseAllFileComplete()
+    {
+        FileUtil.WriteFile(Path.Combine(PathUtil.ReadWritePath, AppConst.FileListName), m_ReadPathFileListData);
+        CheckUpdate();
+    }
+
+    private void OnReleaseFileComplete(DownFileInfo fileInfo)
+    {
+        Debug.Log("OnReleaseFileComplete:" + fileInfo.url);
+        string writeFile = Path.Combine(PathUtil.ReadWritePath, fileInfo.fileName);
+        FileUtil.WriteFile(writeFile, fileInfo.fileData.data);
+        m_DownloadCount++;
+        loadingUI.UpdateProgress(m_DownloadCount);
+
+    }
+
+    private void CheckUpdate()
+    {
+        string url = Path.Combine(AppConst.ResourcesUrl, AppConst.FileListName);
+        DownFileInfo info = new DownFileInfo();
+        info.url = url;
+        StartCoroutine(DownLoadFile(info, OnDownLoadServerFileListComplete));
+    }
+
+    private void OnDownLoadServerFileListComplete(DownFileInfo file)
+    {
+        m_DownloadCount = 0;
+        m_ServerFileListData = file.fileData.data;
+        List<DownFileInfo> fileInfos = GetFileList(file.fileData.text, AppConst.ResourcesUrl);
+        List<DownFileInfo> downListFiles = new List<DownFileInfo>();
+
+        for (int i = 0; i < fileInfos.Count; i++)
+        {
+            string localFile = Path.Combine(PathUtil.ReadWritePath, fileInfos[i].fileName);
+
+            //通过文件的md5来进行校验
+            if (!FileUtil.IsExists(localFile))
+            {
+                fileInfos[i].url = Path.Combine(AppConst.ResourcesUrl, fileInfos[i].fileName);
+                downListFiles.Add(fileInfos[i]);
+            }
+        }
+        if (downListFiles.Count > 0)
+        {
+            StartCoroutine(DownLoadFile(fileInfos, OnUpdateFileComplete, OnUpdateAllFileComplete));
+            loadingUI.InitProgress(downListFiles.Count, "正在更新...");
+        }
+        else
+            EnterGame();
+    }
+
+    private void OnUpdateAllFileComplete()
+    {
+        FileUtil.WriteFile(Path.Combine(PathUtil.ReadWritePath, AppConst.FileListName), m_ServerFileListData);
+        EnterGame();
+        loadingUI.InitProgress(0, "正在载入...");
+    }
+
+    private void OnUpdateFileComplete(DownFileInfo file)
+    {
+        Debug.Log("OnUpdateFileComplete:" + file.url);
+        string writeFile = Path.Combine(PathUtil.ReadWritePath, file.fileName);
+        FileUtil.WriteFile(writeFile, file.fileData.data);
+        m_DownloadCount++;
+        loadingUI.UpdateProgress(m_DownloadCount);
+    }
+	
+	private void EnterGame()
+    {
+		Manager.Event.Fire((int)GameEvent.GameInit);
+		Destroy(loadingObj);
+	}
+}
---
+++ Assets/Scripts/Framework/Manager/UIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //缓存UI
+    //Dictionary<string, GameObject> m_UI = new Dictionary<string, GameObject>();
+
+    //ui分组
+    Dictionary<string, Transform> m_UIGroups = new Dictionary<string, Transform>();
+
+    private Transform m_UIParent;
+
+    private void Awake()
+    {
+        m_UIParent = this.transform.parent.Find("UI");
+    }
+
+    public void SetUIGroup(List<string> group)
+    {
+        for (int i = 0; i < group.Count; i++)
+        {
+            GameObject go = new GameObject("Group-" + group[i]);
+            go.transform.SetParent(m_UIParent,false);
+            m_UIGroups.Add(group[i], go.transform);
+        }
+    }
+
+    Transform GetUIGroup(string group)
+    {
+        if (!m_UIGroups.ContainsKey(group))
+            Debug.LogError("group is not exist");
+        return m_UIGroups[group];
+    }
+
+
+    public void OpenUI(string uiName,string group, string luaName)
+    {
+        GameObject ui = null;
+        Transform parent = GetUIGroup(group);
+        string uiPath = PathUtil.GetUIPath(uiName);
+        Object uiObj = Manager.Pool.Spawn("UI", uiPath);
+        if (uiObj != null)
+        {
+            ui = uiObj as GameObject;
+            ui.transform.SetParent(parent, false);
+
+            UILogic uiLogic = ui.GetComponent<UILogic>();
+            uiLogic.OnOpen();
+            return;
+        }
+
+        Manager.Resource.LoadUI(uiName, (UnityEngine.Object obj) =>
+        {
+            ui = Instantiate(obj) as GameObject;
+            ui.transform.SetParent(parent, false);
+            UILogic uiLogic = ui.AddComponent<UILogic>();
+            uiLogic.AssetName = uiPath;
+            uiLogic.Init(luaName);
+            uiLogic.OnOpen();
+        });
+    }
+
+}
