c71e988ba1344606f6c5fae81c74b18cca58145d
UvUae <1873099584@qq.com>
2025-09-16 10:37:59 +0800

    update

--- Assets/Scripts/Editor/BuildTool.cs
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using System.IO;
+using System.Linq;
 using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
 
 public class BuildTool : Editor
 {
@@ -30,6 +31,8 @@ public class BuildTool : Editor
    {
         List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
 
+        //文件信息列表
+        List<string> bundleInfos = new List<string>();
 
         string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories);
         for (int i = 0; i < files.Length; i++)
@@ -46,8 +49,19 @@ public class BuildTool : Editor
             string bundleName = assetName.Replace("Assets/BuildResources/", "").Replace(Path.GetExtension(assetName), "").ToLower();
             assetBundle.assetBundleName = bundleName+".ab";
             assetBundleBuilds.Add(assetBundle);
+
+            //添加文件和依赖信息
+            List<string> dependenceInfo = GetDependence(assetName);
+            string bundleInfo = assetName + "|" + bundleName + ".ab";
+
+            if (dependenceInfo.Count > 0)
+                bundleInfo = bundleInfo + "|" + string.Join("|", dependenceInfo);
+
+            bundleInfos.Add(bundleInfo);
         }
-        if(Directory.Exists(PathUtil.BundleOutPath)==false)
+
+        //删除旧的bundle目录，重新创建
+        if (Directory.Exists(PathUtil.BundleOutPath)==false)
         {
             Directory.CreateDirectory(PathUtil.BundleOutPath);
         }
@@ -57,7 +71,22 @@ public class BuildTool : Editor
             Directory.CreateDirectory(PathUtil.BundleOutPath);
         }
         BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, target);
+
+        File.WriteAllLines(PathUtil.BundleOutPath + "/" + AppConst.FileListName, bundleInfos);
+
+        AssetDatabase.Refresh();
     }
 
-   
+    /// <summary>
+    /// 获取依赖文件列表
+    /// </summary>
+    /// <param name="curFile"></param>
+    /// <returns></returns>
+    static List<string> GetDependence(string curFile)
+    {
+        List<string> dependence = new List<string>();
+        string[] files = AssetDatabase.GetDependencies(curFile);
+        dependence = files.Where(file => !file.EndsWith(".cs") && !file.Equals(curFile)).ToList();
+        return dependence;
+    }
 }
--- Assets/Scripts/Framework/Util/PathUtil.cs
+++ Assets/Scripts/Framework/Util/PathUtil.cs
@@ -11,6 +11,30 @@ public class PathUtil
     //打包后资源存放目录（bundle输出目录）
     public static readonly string BundleOutPath = Application.streamingAssetsPath;
 
+    //只读目录
+    public static readonly string ReadPath = Application.streamingAssetsPath;
+
+    //可读写目录
+    public static readonly string ReadWritePath = Application.persistentDataPath;
+
+    //lua路径
+    public static readonly string LuaPath = "Assets/BuildResources/LuaScripts";
+
+    //bundle资源路径
+    public static string BundleResourcePath
+
+    {
+        get
+        {
+            if (AppConst.GameMode == GameMode.UpdateMode)
+                return ReadWritePath;
+            return ReadPath;
+        }
+    }
+
+
+
+
     //<summary>
     //获取Unity的相对路径
     //</summary>
@@ -23,9 +47,56 @@ public class PathUtil
         return path.Substring(path.IndexOf("Assets"));
     }
 
+
+    /// <summary>
+    /// 获取标准路径
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     public static string GetStandardPath(string path)
     {
         if(string.IsNullOrEmpty(path)) return string.Empty;
         return path.Trim().Replace("\\", "/");
-    }   
+    }
+
+    public static string GetLuaPath(string name)
+    {
+        return string.Format("Assets/BuildResources/LuaScripts/{0}.bytes", name);
+    }
+
+    public static string GetUIPath(string name)
+    {
+        return string.Format("Assets/BuildResources/UI/Prefabs/{0}.prefab", name);
+    }
+
+    public static string GetMusicPath(string name)
+    {
+        return string.Format("Assets/BuildResources/Audio/Music/{0}", name);
+    }
+
+    public static string GetSoundPath(string name)
+    {
+        return string.Format("Assets/BuildResources/Audio/Sound/{0}", name);
+    }
+
+    public static string GetEffectPath(string name)
+    {
+        return string.Format("Assets/BuildResources/Effect/Prefabs/{0}.prefab", name);
+    }
+
+    public static string GetModelPath(string name)
+    {
+        return string.Format("Assets/BuildResources/Model/Prefabs/{0}.prefab", name);
+    }
+
+    public static string GetSpritePath(string name)
+    {
+        return string.Format("Assets/BuildResources/Sprites/{0}", name);
+    }
+
+    public static string GetScenePath(string name)
+    {
+        return string.Format("Assets/BuildResources/Scenes/{0}.unity", name);
+    }
+
 }
